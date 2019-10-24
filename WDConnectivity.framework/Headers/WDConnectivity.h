13f7de8acb2bb24ac24414c9c19ffe147341fd5d
Carlos Cáceres Watchman Door <ccaceres@watchman.es>
2019-10-24 14:48:54 +0200

    First Commint

---
+++ WDConnectivity.framework/Headers/WDConnectivity.h
@@ -0,0 +1,22 @@
+//
+//  WDConnectivity.h
+//  WDConnectivity
+//
+//  Created by Carlos Cáceres González on 01/03/2019.
+//  Copyright © 2019 Watchman Door S.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CommonCrypto/CommonDigest.h>
+#import <CommonCrypto/CommonCryptor.h>
+#import <CommonCrypto/CommonKeyDerivation.h>
+
+//! Project version number for WDConnectivity.
+FOUNDATION_EXPORT double WDConnectivityVersionNumber;
+
+//! Project version string for WDConnectivity.
+FOUNDATION_EXPORT const unsigned char WDConnectivityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WDConnectivity/PublicHeader.h>
+
+
